23965d8c834b62c286c291c4d9da84a02518cdda
Diane Crame <dcrame@icloud.com>
2020-03-27 00:56:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -86,7 +86,7 @@ def monopoly_with_fourth_tier
 	    {
 	      :reading_railroad=> 
 	      {
-	        "mortgage_value" => "100"
+	        "mortgage_value" => "$100"
 	      },
 	      :pennsylvania_railroad=> 
 	      {
